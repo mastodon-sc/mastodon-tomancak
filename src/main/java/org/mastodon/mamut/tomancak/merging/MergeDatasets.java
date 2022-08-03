06042c840565e5ef30618fe7389114e4d77d2b62
Matthias Arzt <arzt@mpi-cbg.de>
2022-08-03 14:13:22 +0200

    Happy new year 2022

--- src/main/java/org/mastodon/mamut/tomancak/merging/MergeDatasets.java
+++ src/main/java/org/mastodon/mamut/tomancak/merging/MergeDatasets.java
@@ -2,7 +2,7 @@
  * #%L
  * mastodon-tomancak
  * %%
- * Copyright (C) 2018 - 2021 Tobias Pietzsch
+ * Copyright (C) 2018 - 2022 Tobias Pietzsch
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
