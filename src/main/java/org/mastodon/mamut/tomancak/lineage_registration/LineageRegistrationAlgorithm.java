99974b756ff06991cf963fcaa66155bbb15999ec
Matthias Arzt <arzt@mpi-cbg.de>
2023-01-24 15:01:36 +0100

    LineageRegistrationAlgorithm: add getMapping() method

--- src/main/java/org/mastodon/mamut/tomancak/lineage_registration/LineageRegistrationAlgorithm.java
+++ src/main/java/org/mastodon/mamut/tomancak/lineage_registration/LineageRegistrationAlgorithm.java
@@ -1,9 +1,11 @@
 package org.mastodon.mamut.tomancak.lineage_registration;
 
 import net.imglib2.realtransform.AffineTransform3D;
+
 import org.mastodon.collection.RefList;
 import org.mastodon.collection.RefRefMap;
 import org.mastodon.collection.ref.RefArrayList;
+import org.mastodon.collection.ref.RefRefHashMap;
 import org.mastodon.mamut.model.Model;
 import org.mastodon.mamut.model.ModelGraph;
 import org.mastodon.mamut.model.Spot;
@@ -23,7 +25,10 @@ public class LineageRegistrationAlgorithm
 
 	private final ModelGraph graphB;
 
-	private final RefList< Spot > toBeFlipped;
+	/**
+	 * Map branch starting spots in graphA to branch starting spots in graphB.
+	 */
+	private final RefRefMap< Spot, Spot > map;
 
 	public static void run( Model embryoA, Model embryoB )
 	{
@@ -31,10 +36,11 @@ public class LineageRegistrationAlgorithm
 		ModelGraph graphB = embryoB.getGraph();
 		RefRefMap< Spot, Spot > roots = RootsPairing.pairRoots( graphA, graphB );
 		AffineTransform3D transformAB = EstimateTransformation.estimateScaleRotationAndTranslation( roots );
-		RefList< Spot > toBeFlipped = new LineageRegistrationAlgorithm(
+		LineageRegistrationAlgorithm algorithm = new LineageRegistrationAlgorithm(
 				graphA, graphB,
-				roots, transformAB ).getToBeFlipped();
-		FlipDescendants.flipDescendants( embryoB, toBeFlipped );
+				roots, transformAB );
+		RefList< Spot > flip = algorithm.getToBeFlipped();
+		FlipDescendants.flipDescendants( embryoB, flip );
 	}
 
 	public LineageRegistrationAlgorithm( ModelGraph graphA, ModelGraph graphB, RefRefMap< Spot, Spot > roots,
@@ -43,7 +49,7 @@ public class LineageRegistrationAlgorithm
 		this.transformAB = noOffsetTransform( transformAB );
 		this.graphA = graphA;
 		this.graphB = graphB;
-		this.toBeFlipped = new RefArrayList<>( graphB.vertices().getRefPool() );
+		this.map = new RefRefHashMap<>( graphA.vertices().getRefPool(), graphB.vertices().getRefPool() );
 		for ( Spot rootA : roots.keySet() )
 		{
 			Spot rootB = roots.get( rootA );
@@ -53,19 +59,19 @@ public class LineageRegistrationAlgorithm
 
 	private void matchTree( Spot rootA, Spot rootB )
 	{
+		map.put( rootA, rootB );
 		Spot dividingA = LineageTreeUtils.getBranchEnd( graphA, rootA );
 		Spot dividingB = LineageTreeUtils.getBranchEnd( graphB, rootB );
 		try
 		{
-			if ( dividingA.outgoingEdges().size() != 2 ||
-					dividingB.outgoingEdges().size() != 2 )
+			boolean bothDivide = dividingA.outgoingEdges().size() == 2 &&
+					dividingB.outgoingEdges().size() == 2;
+			if ( !bothDivide )
 				return;
 			double[] directionA = SortTreeUtils.directionOfCellDevision( graphA, dividingA );
 			double[] directionB = SortTreeUtils.directionOfCellDevision( graphB, dividingB );
 			transformAB.apply( directionA, directionA );
 			boolean flip = SortTreeUtils.scalarProduct( directionA, directionB ) < 0;
-			if ( flip )
-				toBeFlipped.add( dividingB );
 			matchChildTree( dividingA, dividingB, 0, flip ? 1 : 0 );
 			matchChildTree( dividingA, dividingB, 1, flip ? 0 : 1 );
 		}
@@ -93,7 +99,58 @@ public class LineageRegistrationAlgorithm
 
 	public RefList< Spot > getToBeFlipped()
 	{
-		return toBeFlipped;
+		Spot ref = graphB.vertexRef();
+		try
+		{
+			RefArrayList< Spot > list = new RefArrayList<>( graphB.vertices().getRefPool() );
+			for ( Spot spotA : map.keySet() )
+			{
+				Spot spotB = map.get( spotA, ref );
+				Spot dividingA = LineageTreeUtils.getBranchEnd( graphA, spotA );
+				Spot dividingB = LineageTreeUtils.getBranchEnd( graphB, spotB );
+				if ( doFlip( dividingA, dividingB ) )
+					list.add( dividingB );
+				graphA.releaseRef( dividingA );
+				graphB.releaseRef( dividingB );
+			}
+			return list;
+		}
+		finally
+		{
+			graphB.releaseRef( ref );
+		}
+	}
+
+	private boolean doFlip( Spot dividingA, Spot dividingB )
+	{
+		Spot refA = graphA.vertexRef();
+		Spot refB = graphB.vertexRef();
+		Spot refB2 = graphB.vertexRef();
+		try
+		{
+			boolean bothDivide = dividingA.outgoingEdges().size() == 2 &&
+					dividingB.outgoingEdges().size() == 2;
+			if ( !bothDivide )
+				return false;
+			Spot firstChildA = dividingA.outgoingEdges().get( 0 ).getTarget( refA );
+			Spot secondChildB = dividingB.outgoingEdges().get( 1 ).getTarget( refB );
+			return map.get( firstChildA, refB2 ).equals( secondChildB );
+		}
+		finally
+		{
+			graphA.releaseRef( refA );
+			graphB.releaseRef( refB );
+			graphB.releaseRef( refB2 );
+		}
+	}
+
+	/**
+	 * @return a {@link RefRefMap} that maps (branch starting) spots in embryoA
+	 * to the matched (branch starting) spots in embryoB.
+	 */
+	public RefRefMap< Spot, Spot > getMapping()
+	{
+		return map;
 	}
 
 	private static AffineTransform3D noOffsetTransform( AffineTransform3D transformAB )
