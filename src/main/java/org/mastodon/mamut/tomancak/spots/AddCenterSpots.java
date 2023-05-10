e1dce454c1cf3eba6ebfb4888dbb38f4cad6b4ef
Matthias Arzt <arzt@mpi-cbg.de>
2023-05-10 13:02:07 +0200

    Improve AddCenterSpots plugin
    1. select the newly added spots and links
    2. set and undo point
    3. acquire the write lock on the graph before changing it.

--- src/main/java/org/mastodon/mamut/tomancak/spots/AddCenterSpots.java
+++ src/main/java/org/mastodon/mamut/tomancak/spots/AddCenterSpots.java
@@ -1,15 +1,20 @@
 package org.mastodon.mamut.tomancak.spots;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import javax.swing.JOptionPane;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.mastodon.collection.RefList;
 import org.mastodon.collection.RefSet;
+import org.mastodon.collection.ref.RefArrayList;
 import org.mastodon.mamut.MamutAppModel;
 import org.mastodon.mamut.model.Link;
 import org.mastodon.mamut.model.ModelGraph;
 import org.mastodon.mamut.model.Spot;
 import org.mastodon.mamut.tomancak.sort_tree.SortTreeUtils;
+import org.mastodon.model.SelectionModel;
 
 public class AddCenterSpots
 {
@@ -23,37 +28,48 @@ public class AddCenterSpots
 		RefSet< Spot > selectedSpots = appModel.getSelectionModel().getSelectedVertices();
 		if ( selectedSpots.isEmpty() )
 			JOptionPane.showMessageDialog( null, "No spots selected." );
-		int numTimepoints = appModel.getMaxTimepoint() + 1;
-		List< double[] > averagePosition = SortTreeUtils.calculateAveragePosition( numTimepoints, selectedSpots );
-		addSpots( appModel, averagePosition );
+		Pair< RefList< Spot >, RefList< Link > > newSpotsAndLinks;
+		ModelGraph graph = appModel.getModel().getGraph();
+		ReentrantReadWriteLock.WriteLock writeLock = graph.getLock().writeLock();
+		writeLock.lock();
+		try
+		{
+			int numTimepoints = appModel.getMaxTimepoint() + 1;
+			List< double[] > averagePosition = SortTreeUtils.calculateAveragePosition( numTimepoints, selectedSpots );
+			newSpotsAndLinks = addSpots( graph, averagePosition );
+		}
+		finally
+		{
+			writeLock.unlock();
+		}
+		appModel.getModel().setUndoPoint();
+		selectSpotsAndLinks( appModel, newSpotsAndLinks.getLeft(), newSpotsAndLinks.getRight() );
 	}
 
-	private static void addSpots( MamutAppModel appModel, List< double[] > averagePosition )
+	private static Pair< RefList< Spot >, RefList< Link > > addSpots( ModelGraph graph, List< double[] > averagePosition )
 	{
-		ModelGraph graph = appModel.getModel().getGraph();
+		RefList< Spot > newSpots = new RefArrayList<>( graph.vertices().getRefPool() );
+		RefList< Link > newLinks = new RefArrayList<>( graph.edges().getRefPool() );
 		Spot ref = graph.vertexRef();
 		Spot ref2 = graph.vertexRef();
 		Link eref = graph.edgeRef();
 		try
 		{
 			double radius = 10;
-			Spot previousSpot = null;
 			for ( int timepoint = 0; timepoint < averagePosition.size(); timepoint++ )
 			{
 				double[] center = averagePosition.get( timepoint );
+				if ( center != null )
+				{
+					Spot spot = graph.addVertex( ref ).init( timepoint, center, radius );
+					newSpots.add( spot );
+				}
+			}
 
-				if ( center == null )
-					continue;
-
-				Spot newSpot = graph.addVertex( ref );
-				newSpot.init( timepoint, center, radius );
-
-				if ( previousSpot == null )
-					previousSpot = ref2;
-				else
-					graph.addEdge( previousSpot, newSpot, eref ).init();
-
-				previousSpot.refTo( newSpot );
+			for ( int i = 0; i < newSpots.size() - 1; i++ )
+			{
+				Link link = graph.addEdge( newSpots.get( i, ref ), newSpots.get( i + 1, ref2 ), eref ).init();
+				newLinks.add( link );
 			}
 		}
 		finally
@@ -62,6 +78,14 @@ public class AddCenterSpots
 			graph.releaseRef( ref2 );
 			graph.releaseRef( eref );
 		}
+		return Pair.of( newSpots, newLinks );
 	}
 
+	private static void selectSpotsAndLinks( MamutAppModel appModel, RefList< Spot> spots, RefList< Link> links )
+	{
+		SelectionModel< Spot, Link > selectionModel = appModel.getSelectionModel();
+		selectionModel.clearSelection();
+		selectionModel.setVerticesSelected( spots, true );
+		selectionModel.setEdgesSelected( links, true );
+	}
 }
