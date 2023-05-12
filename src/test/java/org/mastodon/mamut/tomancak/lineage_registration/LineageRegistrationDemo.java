5daae50268f17a80cec4ee377c771853c15c4fb7
Matthias Arzt <arzt@mpi-cbg.de>
2023-05-12 15:58:47 +0200

    LineageRegistrationDemo: improve the demo class
    Don't load the gui state. But open a "TrackScheme Branch" and
    main window (with nice a window titles) for both projects.

--- src/test/java/org/mastodon/mamut/tomancak/lineage_registration/LineageRegistrationDemo.java
+++ src/test/java/org/mastodon/mamut/tomancak/lineage_registration/LineageRegistrationDemo.java
@@ -3,22 +3,25 @@ package org.mastodon.mamut.tomancak.lineage_registration;
 import java.io.IOException;
 
 import mpicbg.spim.data.SpimDataException;
+
+import org.apache.commons.io.FilenameUtils;
 import org.mastodon.mamut.MainWindow;
 import org.mastodon.mamut.WindowManager;
 import org.mastodon.mamut.project.MamutProject;
 import org.mastodon.mamut.project.MamutProjectIO;
+import org.mastodon.views.trackscheme.display.TrackSchemeFrame;
 import org.scijava.Context;
 
 public class LineageRegistrationDemo
 {
+	public static final String project1 = "/home/arzt/Desktop/mastodon-test/project1.mastodon";
+	public static final String project2 = "/home/arzt/Desktop/mastodon-test/project2.mastodon";
 
 	public static void main( String... args )
 	{
 		Context context = new Context();
-		WindowManager projectA = openAppModel( context, "/home/arzt/Datasets/Mette/E1.mastodon" );
-		WindowManager projectB = openAppModel( context, "/home/arzt/Datasets/Mette/E2.mastodon" );
-		projectA.createTrackScheme();
-		projectB.createTrackScheme();
+		openAppModel( context, "/home/arzt/Datasets/Mette/E1.mastodon" );
+		openAppModel( context, "/home/arzt/Datasets/Mette/E2.mastodon" );
 		context.service( LineageRegistrationControlService.class ).showDialog();
 	}
 
@@ -29,8 +32,13 @@ public class LineageRegistrationDemo
 		{
 			MamutProject project = new MamutProjectIO().load( projectPath );
 			WindowManager wm = new WindowManager( context );
-			wm.getProjectManager().open( project );
-			new MainWindow( wm ).setVisible( true );
+			wm.getProjectManager().open( project, false, true );
+			TrackSchemeFrame frame = wm.createBranchTrackScheme().getFrame();
+			String baseName = FilenameUtils.getBaseName( projectPath );
+			frame.setTitle( frame.getTitle() + " " + baseName );
+			MainWindow mainWindow = new MainWindow( wm );
+			mainWindow.setVisible( true );
+			mainWindow.setTitle( mainWindow.getTitle() + " " + baseName );
 			return wm;
 		}
 		catch ( SpimDataException | IOException e )
