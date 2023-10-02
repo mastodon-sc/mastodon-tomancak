d14608b63b3d5005f070c23980304c97473d26fe
Matthias Arzt <arzt@mpi-cbg.de>
2023-10-02 15:01:23 +0200

    MastodonGitNewBranch: better title

--- src/main/java/org/mastodon/mamut/tomancak/collaboration/commands/MastodonGitNewBranch.java
+++ src/main/java/org/mastodon/mamut/tomancak/collaboration/commands/MastodonGitNewBranch.java
@@ -1,10 +1,11 @@
 package org.mastodon.mamut.tomancak.collaboration.commands;
 
-import org.mastodon.mamut.WindowManager;
 import org.mastodon.mamut.tomancak.collaboration.MastodonGitRepository;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
 
+@Plugin( type = Command.class, label = "Create New Branch", visible = false )
 public class MastodonGitNewBranch implements Command
 {
 
