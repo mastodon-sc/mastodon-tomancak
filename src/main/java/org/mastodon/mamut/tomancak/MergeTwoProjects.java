b89aca252333955f96f7bcc0ee9456332fa4919f
Matthias Arzt <arzt@mpi-cbg.de>
2024-10-10 15:33:29 +0200

    Refactor MergeTwoProjects
    Rearrange code & and set window close operation to DISPOSE_ON_CLOSE to
    avoid garbage collection problems

--- src/main/java/org/mastodon/mamut/tomancak/MergeTwoProjects.java
+++ src/main/java/org/mastodon/mamut/tomancak/MergeTwoProjects.java
@@ -11,47 +11,47 @@ import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-@Plugin( type = Command.class, label = "Mastodon - Merge two mastodon projects into one project", menuPath = "Plugins > Tracking > Mastodon > Merge Two Projects" )
+@Plugin( type = Command.class, menuPath = "Plugins > Tracking > Mastodon > Merge Two Projects" )
 public class MergeTwoProjects implements Command
 {
 	@Parameter
 	private Context context;
 
+	private MergingDialog mergingDialog;
+
 	@Override
 	public void run()
 	{
-		mergeProjects();
+		if ( mergingDialog == null )
+		{
+			mergingDialog = new MergingDialog( null );
+			mergingDialog.onMerge( this::mergeProjects );
+		}
+		mergingDialog.setVisible( true );
 	}
-
-	private MergingDialog mergingDialog;
 
 	private void mergeProjects()
 	{
-		if ( mergingDialog == null )
-			mergingDialog = new MergingDialog( null );
-		mergingDialog.onMerge( () -> {
-			try
-			{
-				final String pathA = mergingDialog.getPathA();
-				final String pathB = mergingDialog.getPathB();
-				final double distCutoff = mergingDialog.getDistCutoff();
-				final double mahalanobisDistCutoff = mergingDialog.getMahalanobisDistCutoff();
-				final double ratioThreshold = mergingDialog.getRatioThreshold();
+		try
+		{
+			final String pathA = mergingDialog.getPathA();
+			final String pathB = mergingDialog.getPathB();
+			final double distCutoff = mergingDialog.getDistCutoff();
+			final double mahalanobisDistCutoff = mergingDialog.getMahalanobisDistCutoff();
+			final double ratioThreshold = mergingDialog.getRatioThreshold();
 
-				final Dataset dsA = new Dataset( pathA );
-				final Dataset dsB = new Dataset( pathB );
+			final Dataset dsA = new Dataset( pathA );
+			final Dataset dsB = new Dataset( pathB );
 
-				final ProjectModel projectMerged = ProjectCreator.createProjectFromBdvFile( dsA.project().getDatasetXmlFile(), context );
-				final MergeDatasets.OutputDataSet output = new MergeDatasets.OutputDataSet( projectMerged.getModel() );
-				MergeDatasets.merge( dsA, dsB, output, distCutoff, mahalanobisDistCutoff, ratioThreshold );
-				// start a new instance of Mastodon that shows the result of the merge operation
-				new MainWindow( projectMerged ).setVisible( true );
-			}
-			catch ( final Exception e )
-			{
-				e.printStackTrace();
-			}
-		} );
-		mergingDialog.setVisible( true );
+			final ProjectModel projectMerged = ProjectCreator.createProjectFromBdvFile( dsA.project().getDatasetXmlFile(), context );
+			final MergeDatasets.OutputDataSet output = new MergeDatasets.OutputDataSet( projectMerged.getModel() );
+			MergeDatasets.merge( dsA, dsB, output, distCutoff, mahalanobisDistCutoff, ratioThreshold );
+			// start a new instance of Mastodon that shows the result of the merge operation
+			new MainWindow( projectMerged ).setVisible( true );
+		}
+		catch ( final Exception e )
+		{
+			e.printStackTrace();
+		}
 	}
 }
